5976860be16288741a03bbeb975973be313c9a87
Rafael <faelmota7.gmail.com>
2022-09-30 22:46:09 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int numero;
+Console.Write("\nDigite um número para análise: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("\nPositivo");
+        Console.ResetColor();
+    }
+else if (numero < 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\nNegativo");
+        Console.ResetColor();
+
+    }
+else if (numero == 0)
+    {
+        Console.WriteLine("\nZero\n");
+    }
+
+
+
+
+
+
+
