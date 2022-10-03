081cc06c75ac2c04857b067ac98a1374ea11187e
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-10-03 19:43:59 -0300

    Resolução

--- Program.cs
+++ Program.cs
@@ -5,24 +5,19 @@ numero = Convert.ToInt32(Console.ReadLine());
 if (numero > 0)
     {
         Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine("\nPositivo");
+        Console.WriteLine("\nPositivo\n");
         Console.ResetColor();
     }
 else if (numero < 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\nNegativo");
-        Console.ResetColor();
+     {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nNegativo\n");
+            Console.ResetColor();
 
-    }
+      }
 else if (numero == 0)
-    {
-        Console.WriteLine("\nZero\n");
-    }
+            {
 
-
-
-
-
-
-
+                Console.WriteLine("\nZero\n");
+            }
+            
